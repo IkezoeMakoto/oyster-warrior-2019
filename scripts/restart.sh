122f7453478e80b40276c19eff57b149d97b3cc2
IkezoeMakoto <150283t@gmail.com>
2019-09-08 17:32:56 +0900

    3台構成にした

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ## 並列でデプロイする
 
 # server1
-cat ${SCRIPT_DIR}/deploy/app.sh | ssh isucon-server1 &
+cat ${SCRIPT_DIR}/deploy/app.sh ${SCRIPT_DIR}/deploy/web.sh | ssh isucon-server1 &
 # server2
-cat ${SCRIPT_DIR}/deploy/web.sh | ssh isucon-server1 &
+cat ${SCRIPT_DIR}/deploy/app.sh | ssh isucon-server2 &
 # server3
-cat ${SCRIPT_DIR}/deploy/database.sh | ssh isucon-server1 &
+cat ${SCRIPT_DIR}/deploy/app.sh ${SCRIPT_DIR}/deploy/database.sh | ssh isucon-server3 &
 
 wait

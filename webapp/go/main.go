a86b0da74cd53bdf38f7dff6816e3e73c64b568b
02 <cocoeyes02@gmail.com>
2019-09-08 14:11:53 +0900

    fix: クエリにミスがあったので修正

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1067,7 +1067,7 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 		// 初期ページに表示するときに必要なデータを取得
 		// 1st page
 		rows, err := tx.Queryx("SELECT items.id as item_id,items.seller_id,items.buyer_id,items.status,items.name,items.price,items.description,items.image_name,items.category_id,items.created_at as item_created_at,items.updated_at," +
-			"users.id as user_id, users.account_name, users.num_sell_items" +
+			"users.id as user_id, users.account_name, users.num_sell_items " +
 			"FROM `items` " +
 			"INNER JOIN `users` on `items`.seller_id = `users`.id " +
 			"WHERE (`seller_id` = ? OR `buyer_id` = ?) " +
